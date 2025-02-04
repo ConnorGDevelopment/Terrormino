56f8f19964764aae4d72e2de0c337fc76c0a458b
EricPopp76 <157061237+EricPopp76@users.noreply.github.com>
2025-02-03 20:07:40 -0500

    figuring out some xr toolkit things

--- Assets/Flashlight/FlashlightScript.cs
+++ Assets/Flashlight/FlashlightScript.cs
@@ -2,17 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class FlashlightScript : MonoBehaviour
 {
     [SerializeField] private GameObject FlashlightLight; // Assign your flashlight GameObject in the Inspector
     private bool FlashlightActive = false;
 
+
+    private UnityEngine.XR.InputDevice _device_leftController;
+    private UnityEngine.XR.InputDevice _device_rightController;
+
+    private Vector3 _inputVelocity_leftController;
+    private Vector3 _inputVelocity_rightController;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+
+
+        _device_leftController = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+        _device_rightController = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+
         // Ensure the flashlight starts off
         FlashlightLight.SetActive(false);
+    }
+    void Update()
+    {
+        getInput();
+
+        Debug.Log(_inputVelocity_leftController.ToString());
+        Debug.Log (_inputVelocity_rightController.ToString());
+
+    }
+
+    public void getInput()
+    {
+        _device_leftController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.deviceVelocity, out _inputVelocity_leftController);
+        _device_rightController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.deviceVelocity, out _inputVelocity_rightController);
     }
 
     public void FlashLightButton(InputAction.CallbackContext context)
