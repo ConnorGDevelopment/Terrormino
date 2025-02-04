ec6aa2fffe2d61a2dd18138991ac46b8905250c1
EricPopp76 <157061237+EricPopp76@users.noreply.github.com>
2025-02-04 10:55:12 -0500

    Updated flashlight

--- Assets/Flashlight/FlashlightScript.cs
+++ Assets/Flashlight/FlashlightScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR;
@@ -11,11 +12,14 @@ public class FlashlightScript : MonoBehaviour
     private bool FlashlightActive = false;
 
 
-
+    private bool inHand = false;
     private InputData _inputData;
 
-    private float timer = 10f;
+    private float _pastTime = 0.5f;
+    private float _battery = 10f;
 
+    private Vector3 PastLeftVelocity = Vector3.zero;
+    private Vector3 PastRightVelocity = Vector3.zero;
 
 
     // Start is called before the first frame update
@@ -30,47 +34,82 @@ public class FlashlightScript : MonoBehaviour
     }
     void Update()
     {
+        if (inHand)  // Flashlight battery drains when held
+        {
+            _battery -= Time.deltaTime;
+        }
 
-        timer = timer - Time.deltaTime;
-
-        if (timer < 0)
+        if (_battery < 0) // Battery dies
         {
             FlashLightLight.SetActive(false);
         }
 
+        _pastTime -= Time.deltaTime;
 
-        
-        if(_inputData._leftController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.deviceVelocity, out Vector3 leftVelocity))
+        // Store past velocity every 0.5s
+        if (_pastTime <= 0)
         {
-            Debug.Log(leftVelocity.magnitude.ToString());
-            if (leftVelocity.magnitude > 1)
+            if (_inputData._leftController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.deviceVelocity, out Vector3 currentLeftVelocity))
             {
-                timer = 10f;
+                PastLeftVelocity = currentLeftVelocity;
+            }
+
+            if (_inputData._rightController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.deviceVelocity, out Vector3 currentRightVelocity))
+            {
+                PastRightVelocity = currentRightVelocity;
+            }
+
+            _pastTime = 0.5f; // Reset the timer
+        }
+
+        // Measure change in left controller velocity
+        if (_inputData._leftController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.deviceVelocity, out Vector3 leftVelocity))
+        {
+            float LeftMagnitudeChange = Mathf.Abs(leftVelocity.magnitude - PastLeftVelocity.magnitude);
+            Debug.Log($"Left Magnitude Change: {LeftMagnitudeChange}");
+
+            if (LeftMagnitudeChange > 0.5f) // Sensitivity threshold
+            {
+                _battery = 10f;
             }
         }
 
-        if (_inputData._leftController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.deviceVelocity, out Vector3 rightVelocity))
-
+        // Measure change in right controller velocity
+        if (_inputData._rightController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.deviceVelocity, out Vector3 rightVelocity))
         {
-            Debug.Log(rightVelocity.magnitude.ToString());
+            float RightMagnitudeChange = Mathf.Abs(rightVelocity.magnitude - PastRightVelocity.magnitude);
+            //Debug.Log($"Right Magnitude Change: {RightMagnitudeChange}");
 
-            if(rightVelocity.magnitude > 1)
+            if (RightMagnitudeChange > 0.5f)
             {
-                timer = 10f;
+                _battery = 10f;
             }
         }
-
-
-
-
     }
 
-   
 
+
+
+
+//Grabbing the flashlight
+public void OnFlashlightGrab(SelectEnterEventArgs _)  
+    {
+        inHand = true;
+    }
+
+    //Dropping flashlight
+    public void OnFlashlightRelease(SelectExitEventArgs _)
+    {
+        inHand = false;
+    }
+
+
+   
+    //Checking if the primary button on either controller is in hand
     public void FlashLightButton(InputAction.CallbackContext context)
     {
         // Check if the button is pressed 
-        if (context.performed)
+        if (context.performed && inHand)
         {
             
             
