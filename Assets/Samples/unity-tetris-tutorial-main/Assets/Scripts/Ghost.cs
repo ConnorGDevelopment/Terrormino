d91a42424fc9a724b1f3b331eed81035d5785080
ConnorGDevelopment <connorgdevelopment@gmail.com>
2025-03-18 10:12:40 -0400

    Unborked

--- Assets/Samples/unity-tetris-tutorial-main/Assets/Scripts/Ghost.cs
+++ Assets/Samples/unity-tetris-tutorial-main/Assets/Scripts/Ghost.cs
@@ -1,76 +1,76 @@
-using UnityEngine;
-using UnityEngine.Tilemaps;
+//using UnityEngine;
+//using UnityEngine.Tilemaps;
 
-public class Ghost : MonoBehaviour
-{
-    public Tile tile;
-    public Board mainBoard;
-    public Piece trackingPiece;
+//public class Ghost : MonoBehaviour
+//{
+//    public Tile tile;
+//    public Board mainBoard;
+//    public Piece trackingPiece;
 
-    public Tilemap tilemap { get; private set; }
-    public Vector3Int[] cells { get; private set; }
-    public Vector3Int position { get; private set; }
+//    public Tilemap tilemap { get; private set; }
+//    public Vector3Int[] cells { get; private set; }
+//    public Vector3Int position { get; private set; }
 
-    private void Awake()
-    {
-        tilemap = GetComponentInChildren<Tilemap>();
-        cells = new Vector3Int[4];
-    }
+//    private void Awake()
+//    {
+//        tilemap = GetComponentInChildren<Tilemap>();
+//        cells = new Vector3Int[4];
+//    }
 
-    private void LateUpdate()
-    {
-        Clear();
-        Copy();
-        Drop();
-        Set();
-    }
+//    private void LateUpdate()
+//    {
+//        Clear();
+//        Copy();
+//        Drop();
+//        Set();
+//    }
 
-    private void Clear()
-    {
-        for (int i = 0; i < cells.Length; i++)
-        {
-            Vector3Int tilePosition = cells[i] + position;
-            tilemap.SetTile(tilePosition, null);
-        }
-    }
+//    private void Clear()
+//    {
+//        for (int i = 0; i < cells.Length; i++)
+//        {
+//            Vector3Int tilePosition = cells[i] + position;
+//            tilemap.SetTile(tilePosition, null);
+//        }
+//    }
 
-    private void Copy()
-    {
-        for (int i = 0; i < cells.Length; i++) {
-            cells[i] = trackingPiece.cells[i];
-        }
-    }
+//    private void Copy()
+//    {
+//        for (int i = 0; i < cells.Length; i++) {
+//            cells[i] = trackingPiece.cells[i];
+//        }
+//    }
 
-    private void Drop()
-    {
-        Vector3Int position = trackingPiece.position;
+//    private void Drop()
+//    {
+//        Vector3Int position = trackingPiece.position;
 
-        int current = position.y;
-        int bottom = -mainBoard.boardSize.y / 2 - 1;
+//        int current = position.y;
+//        int bottom = -mainBoard.boardSize.y / 2 - 1;
 
-        mainBoard.Clear(trackingPiece);
+//        mainBoard.Clear(trackingPiece);
 
-        for (int row = current; row >= bottom; row--)
-        {
-            position.y = row;
+//        for (int row = current; row >= bottom; row--)
+//        {
+//            position.y = row;
 
-            if (mainBoard.IsValidPosition(trackingPiece, position)) {
-                this.position = position;
-            } else {
-                break;
-            }
-        }
+//            if (mainBoard.IsValidPosition(trackingPiece, position)) {
+//                this.position = position;
+//            } else {
+//                break;
+//            }
+//        }
 
-        mainBoard.Set(trackingPiece);
-    }
+//        mainBoard.Set(trackingPiece);
+//    }
 
-    private void Set()
-    {
-        for (int i = 0; i < cells.Length; i++)
-        {
-            Vector3Int tilePosition = cells[i] + position;
-            tilemap.SetTile(tilePosition, tile);
-        }
-    }
+//    private void Set()
+//    {
+//        for (int i = 0; i < cells.Length; i++)
+//        {
+//            Vector3Int tilePosition = cells[i] + position;
+//            tilemap.SetTile(tilePosition, tile);
+//        }
+//    }
 
-}
+//}
