fcb9c5b5db2efe651940f7ec5dcdcec5524ff52d
EricPopp76 <157061237+EricPopp76@users.noreply.github.com>
2025-04-15 09:37:56 -0400

    some fixes to grabbables

--- Assets/Demon/JumpscareTrigger.cs
+++ Assets/Demon/JumpscareTrigger.cs
@@ -1,59 +1,74 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class JumpscareTrigger : MonoBehaviour
 {
     public AudioSource Scream;
-    
-    public GameObject Jumpscare;
-    
+
+    public GameObject JumpscareDemon;
+    public GameObject Bedroom;
 
 
+    private Player.Manager _playerManager;
+    private Demon.Manager _demonManager;
+    
 
     public Light MoonLight;
-    
+
 
     private void Start()
     {
-       
+
+        
+
+
     }
 
-    private void OnTriggerEnter(Collider other)
+
+
+
+
+    public UnityEvent Jumpscare = new();
+
+    
+
+    public void OnJumpscare()
     {
-        if(other.CompareTag("Enemy"))
-        {
-            
-            Debug.Log("jumpscare");
-            Scream.Play();
-            Jumpscare.SetActive(true);
-            AdjustingMoonlight();
-            other.gameObject.SetActive(false);
-            StartCoroutine(EndJumpscare());
-        }        
+        
+        _demonManager._demons.ForEach(demon => demon.SetActive(false));
+        Scream.Play();
+        Bedroom.SetActive(false);
+        JumpscareDemon.SetActive(true);
+
+
+        StartCoroutine(EndJumpscare());
     }
+
 
     IEnumerator EndJumpscare()
     {
         yield return new WaitForSeconds(1.5f);
         Scream.Stop();
-        Jumpscare.SetActive(false);
+        JumpscareDemon.SetActive(false);
         SceneManager.LoadScene("TitleScreen");
-        
-
     }
 
 
-    public void AdjustingMoonlight()
+
+
+    public void Awake()
     {
-       
-        
-        MoonLight.enabled = false;
-        
-        
-    }
+        _playerManager = Helpers.Debug.TryFindByTag("Player").GetComponent<Player.Manager>();
+        if (_playerManager != null)
+        {
+            _playerManager.GameOver.AddListener(OnJumpscare);
+        }
+        _demonManager = Helpers.Debug.TryFindByTag("DemonManager").GetComponent<Demon.Manager>();
 
+        Jumpscare.AddListener(OnJumpscare);
+    }
 
 
 
--- Assets/Player/Player.Manager.cs
+++ Assets/Player/Player.Manager.cs
@@ -1,27 +1,40 @@
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 namespace Player
 {
     public class Manager : MonoBehaviour
     {
+        public JumpscareTrigger Jumpscaretrigger;
         public UnityEvent GameOver = new();
 
-        public static void OnGameOver()
+        public void OnGameOver()
         {
             Debug.Log("Game Over");
         }
 
+        public void BackToTitle()
+        {
+            SceneManager.LoadScene("TitleScreen");
+        }
+
         public void Start()
         {
+
+            Jumpscaretrigger = Helpers.Debug.TryFindComponentOnGameObjectByTag<JumpscareTrigger>("Player");
+
             GameOver.AddListener(OnGameOver);
         }
 
         public void OnTriggerEnter(Collider other)
         {
-            if (other.CompareTag("Enemy"))
+            if (other.TryGetComponent(out Demon.LightFear _))
             {
+                Debug.Log(other.gameObject);
+                //EditorApplication.isPaused = true;
                 GameOver.Invoke();
+                Jumpscaretrigger.Jumpscare.Invoke();
             }
         }
     }
