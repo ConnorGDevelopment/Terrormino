e3b5870a1ccb7dd860a2362a78e9071af3f68dbf
EricPopp76 <157061237+EricPopp76@users.noreply.github.com>
2025-04-16 20:21:03 -0400

    begun work on player looking direction

---
+++ Assets/Player/Player.LookDirection.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class LookDirection : MonoBehaviour
+    {
+
+
+        public Collider PlayerLookDirection;
+        
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+
+        public void OnTriggerEnter(Collider other)
+        {
+            
+        }
+
+
+        public void ObjectFreeze(GameObject other)
+        {
+            
+        }
+
+
+
+    }
+}
