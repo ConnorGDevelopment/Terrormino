5ae79f496ab2bd47ba4d36661ae8b2c4b00df520
EricPopp76 <thunderhawks76@gmail.com>
2025-04-22 16:49:02 -0700

    Almost finished ambient objects script
    Objects should move around the room and they should stop if the players looks at them.

--- Assets/Ambient/Ambient.Manager.cs
+++ Assets/Ambient/Ambient.Manager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 namespace Ambient
@@ -9,6 +10,8 @@ namespace Ambient
         public List<GameObject> TrackedObjects;
         public Collider PlayerVision;
 
+        private float timer = 0f; //this is a timer to check how frequently we can do effect stuff
+                                  //without this, the effect stuff happens every frame if the logic is kept in update
         //public float Frequency;
         //private float _timer = 0;
         //public void Update()
@@ -23,13 +26,31 @@ namespace Ambient
         //}
         public void Update()
         {
+            timer += Time.deltaTime;
+
+            if (timer >= 5f)
+            {
+                TriggerEffects();
+                timer = 0f;
+            }
+
+        }
+
+        public void Start()
+        {
+            Effects.AddRange(gameObject.GetComponents<Effect>());
+        }
+
+
+        void TriggerEffects()
+        {
             List<GameObject> validObjects = new List<GameObject>();
 
             foreach (GameObject trackedObject in TrackedObjects)
             {
                 if (!PlayerVision.bounds.Intersects(trackedObject.GetComponent<Collider>().bounds))
                 {
-                    validObjects.Add(trackedObject);
+                    validObjects.Add(trackedObject); 
                 }
             }
 
@@ -39,9 +60,7 @@ namespace Ambient
             }
         }
 
-        public void Start()
-        {
-            Effects.AddRange(gameObject.GetComponents<Effect>());
-        }
+
+
     }
 }
--- Assets/Ambient/Effects/Ambient.SwapPosition.cs
+++ Assets/Ambient/Effects/Ambient.SwapPosition.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,23 +6,65 @@ namespace Ambient
 {
     public class SwapPosition : Effect
     {
-        public GameObject PrimaryObject;
-        public GameObject SecondaryObject;
+        public List<Transform> SpawnPoints;
+        public float minDelay = 5f; 
+        public float maxDelay = 10f; 
+
+        private List<Transform> _availableSpawnPoints;
+        private HashSet<GameObject> _currentlySwapping = new();
+        public Collider PlayerVision;
 
         public override void OnEnable()
         {
             base.OnEnable();
-            Helpers.Debug.CheckIfSetInInspector(PrimaryObject, "PrimaryObject");
-            Helpers.Debug.CheckIfSetInInspector(SecondaryObject, "SecondaryObject");
         }
 
-        public override void OnTriggerEffect(List<GameObject> _)
+        public override void OnTriggerEffect(List<GameObject> gameObjects)
         {
-            var primaryOldPosition = PrimaryObject.transform.position;
-            var secondaryOldPosition = SecondaryObject.transform.position;
+            
+            _availableSpawnPoints = new List<Transform>(SpawnPoints);
 
-            PrimaryObject.transform.position = secondaryOldPosition;
-            SecondaryObject.transform.position = primaryOldPosition;
+            foreach (var obj in gameObjects)
+            {
+                StartCoroutine(SwapAfterDelay(obj));
+            }
+        }
+
+        private IEnumerator SwapAfterDelay(GameObject obj)
+        {
+            if (_currentlySwapping.Contains(obj))   //Will stop the swap if the player is looking at the obj
+                yield break;
+
+            _currentlySwapping.Add(obj);
+
+            float delay = Random.Range(minDelay, maxDelay);
+            float elapsed = 0f;
+
+            
+            while (elapsed < delay)
+            {
+                if (PlayerVision.bounds.Intersects(obj.GetComponent<Collider>().bounds))
+                {
+                    _currentlySwapping.Remove(obj); 
+                    yield break;
+                }
+
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            if (_availableSpawnPoints.Count == 0)
+            {
+                _currentlySwapping.Remove(obj);
+                yield break;
+            }
+
+            int randomIndex = Random.Range(0, _availableSpawnPoints.Count);
+            Transform chosenSpawn = _availableSpawnPoints[randomIndex];
+            obj.transform.position = chosenSpawn.position;
+
+            _availableSpawnPoints.RemoveAt(randomIndex);
+            _currentlySwapping.Remove(obj);
         }
     }
 }
